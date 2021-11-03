eb83385ee41e99bb81a4e28a7e2fa5c2ff3b0130
Petar Kresoja <petarkresoja5@gmail.com>
2021-11-03 12:08:34 +0100

    Upload

---
+++ cloud-config-service/src/test/java/com/pequla/config/CloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pequla.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/pequla/user/service/UserService.java
@@ -0,0 +1,41 @@
+package com.pequla.user.service;
+
+import com.pequla.user.entity.User;
+import com.pequla.user.repository.UserRepository;
+import com.pequla.user.vo.Department;
+import com.pequla.user.vo.ResponseTemplateVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User saveUser(User user) {
+        log.info("Inside saveUser of UserService");
+        return userRepository.save(user);
+    }
+
+    public ResponseTemplateVO getUserWithDepartment(Long userId) {
+        log.info("Inside getUserWithDepartment of UserService");
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        User user = userRepository.findByUserId(userId);
+
+        Department department =
+                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId()
+                        ,Department.class);
+
+        vo.setUser(user);
+        vo.setDepartment(department);
+
+        return  vo;
+    }
+}
